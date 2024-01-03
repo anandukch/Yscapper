09a6ec3a06e71bc5e408cf568b9313ab479a11e9
anandukch <anandukch@gmail.com>
2024-01-03 20:59:18 +0530

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# pagesoft
+# Yscapper
+
+This project is a web scraping tool built using the core modules of Node.js. It allows you to scrape a webpage without relying on any third-party libraries like Cheerio or Puppeteer. The scraped data is then stored in a JSON file.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/your-username/project-name.git`
+2. Navigate to the project directory: `cd project-name`
+3. Install the dependencies: `npm install`
+
+## Usage
+
+To start the project in development mode, run the following command:
+```shell
+    npm run start:dev
+```
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,94 +1,71 @@
 import * as http from "https";
 import * as fs from "fs";
 
-const url ="https://news.ycombinator.com/"
+const results = {
+  "0-100": [],
+  "101-200": [],
+  "201-300": [],
+  "301-n": [],
+};
 
-http
-  .get(url, (res) => {
-    let data = "";
+const scrapeUrl = (url: string) => {
+  return new Promise((resolve, reject) => {
+    http
+      .get(url, (res) => {
+        let data = "";
 
-    res.on("data", (chunk) => {
-      data += chunk;
-    });
-
-    res.on("end", () => {
-      const rankRegex = /<span class="rank">(\d+)/;
-      const titleRegex =
-        /<span class="titleline">(?:<a [^>]*href=".*?">)?(.*?)(?:<)/;
-      const pointsRegex = /<span class="score" id="score_\d+">(\d+)/;
-      const authorRegex = /<a href="user\?id=.*?" class="hnuser">(.*?)</;
-      const commentsRegex = /<a href="item\?id=\d+">(\d+)&nbsp;comment/;
-
-      // <a href="item?id=38840747">9&nbsp;comments</a>
-
-      const results = {
-        "0-100": [],
-        "101-200": [],
-        "201-300": [],
-        "301-n": [],
-      };
-
-      // Extract information using regular expressions
-      let itemRes = "";
-      data
-        .split('<tr class="spacer" style="height:5px"></tr>')
-        .forEach((item, i) => {
-          // console.log(item);
-
-          itemRes += item + "\n\n\n\n";
-
-          const rank = item.match(rankRegex) || null;
-          const title = item.match(titleRegex) || null;
-          const points = item.match(pointsRegex) || null;
-          const author = item.match(authorRegex) || null;
-          const comments = item.match(commentsRegex);
-
-          // console.log(i,
-          //   comments[1],
-          //   rank[1],
-          //   title[1],
-          //   points[1],
-          //   author[1]);
-
-          if (comments) {
-            const commentsCount = parseInt(comments[1], 10);
-            const processedData = processData(
-              rank,
-              title,
-              points,
-              author,
-              comments
-            );
-
-            if (commentsCount >= 0 && commentsCount <= 100) {
-              results["0-100"].push(processedData);
-            } else if (commentsCount >= 101 && commentsCount <= 200) {
-              results["101-200"].push(processedData);
-            } else if (commentsCount >= 201 && commentsCount <= 300) {
-              results["201-300"].push(processedData);
-            } else {
-              results["301-n"].push(processedData);
-            }
-          }
+        res.on("data", (chunk) => {
+          data += chunk;
         });
-      fs.writeFileSync("item.txt", itemRes);
 
-      const jsonResult = JSON.stringify(results, null, 2);
-      fs.writeFileSync("results.json", jsonResult);
-      console.log("Results saved as results.json");
-    });
+        res.on("end", () => {
+          // Regex to match the rank, title, points, author and comments
+          const rankRegex = /<span class="rank">(\d+)/;
+          const titleRegex =
+            /<span class="titleline">(?:<a [^>]*href=".*?">)?(.*?)(?:<)/;
+          const pointsRegex = /<span class="score" id="score_\d+">(\d+)/;
+          const authorRegex = /<a href="user\?id=.*?" class="hnuser">(.*?)</;
+          const commentsRegex = /<a href="item\?id=\d+">(\d+)&nbsp;comment/;
+          let itemRes = "";
 
-    // fs.writeFile("output.txt", data, (err) => {
-    //   if (err) {
-    //     console.error(`Error writing file: ${err}`);
-    //   } else {
-    //     console.log("Data saved to output.txt");
-    //   }
-    // });
-  })
-  .on("error", (err) => {
-    console.error(`Error: ${err}`);
+          // Split the data into an array of items
+          data
+            .split('<tr class="spacer" style="height:5px"></tr>')
+            .forEach((item, i) => {
+
+              const rank = item.match(rankRegex) || null;
+              const title = item.match(titleRegex) || null;
+              const points = item.match(pointsRegex) || null;
+              const author = item.match(authorRegex) || null;
+              const comments = item.match(commentsRegex) || null;
+              if (comments) {
+                const commentsCount = parseInt(comments[1], 10);
+                const processedData = processData(
+                  rank,
+                  title,
+                  points,
+                  author,
+                  comments
+                );
+
+                if (commentsCount >= 0 && commentsCount <= 100) {
+                  results["0-100"].push(processedData);
+                } else if (commentsCount >= 101 && commentsCount <= 200) {
+                  results["101-200"].push(processedData);
+                } else if (commentsCount >= 201 && commentsCount <= 300) {
+                  results["201-300"].push(processedData);
+                } else {
+                  results["301-n"].push(processedData);
+                }
+              }
+            });
+        });
+      })
+      .on("error", (err) => {
+        console.error(`Error: ${err}`);
+      });
   });
+};
 
 const processData = (
   rank: RegExpMatchArray | null,
@@ -103,3 +80,18 @@ const processData = (
   author: author ? author[1] : null,
   comments: comments ? comments[1] : 0,
 });
+
+const scrapeMultipleUrls = async () => {
+  for (let i = 1; i <= 20; i++) {
+    const url = `https://news.ycombinator.com/?p=${i}`;
+    await scrapeUrl(url);
+  }
+
+  const jsonResult = JSON.stringify(results, null, 2);
+  fs.writeFileSync("results.json", jsonResult);
+  console.log("Results saved as results.json");
+};
+
+scrapeMultipleUrls();
+
+
